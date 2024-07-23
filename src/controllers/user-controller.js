72c57b9f2a28275a61a06256bb22e78067980a49
abhishek sharma <abhisheksharmaqwr0123@gmail.com>
2024-07-23 19:10:07 +0530

    encryption added or signup done

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,32 @@
+const { response }=require('express');
+const UserService=require('../services/user-service');
+
+const userService=new UserService();
+
+const create=async (req,res)=>{
+    try {
+        const response=await userService.create({
+            email: req.body.email,
+            password: req.body.password
+        });
+        return res.status(201).json({
+            succees:true,
+            message:'successfully created a new user ',
+            data:response,
+            err:{}
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message:'something went wrong',
+            data:{},
+            succees:false,
+             err:error
+        });
+    }
+
+}
+module.exports={
+    create
+}
