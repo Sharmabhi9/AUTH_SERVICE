1cd8c465f5bb0e5083f62e0363480081034f0af5
abhishek sharma <abhisheksharmaqwr0123@gmail.com>
2024-07-25 16:29:56 +0530

    Added Roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const bodyParser=require('body-parser');
 const { PORT }=require('./config/serverconfig');
 
 const apiRoutes=require('./routes/index');
- const UserService=require('./services/user-service');
+const UserService=require('./services/user-service');
+const db=require('./models/index');
+const {User,Role}=require('./models/index')
 
  
 const app=express();
@@ -17,6 +19,15 @@ const prepareAndstartserver=()=>{
     app.listen(PORT, async ()=>{
         console.log(`server started on PORT: ${PORT}`);
         const service=new UserService();
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter:true})
+        }
+        const U1=await User.findByPk(2);
+        const R1=await Role.findByPk(1);
+
+        U1.addRole(R1);
+        const response=await U1.hasRole(R1);
+        console.log(response);
         // // const newToken=service.createToken({email:'shiva@gmail.com',id:1});
         // // console.log("new token is ",newToken);
         // const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoaXZhQGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE3MjE3NTg5MDEsImV4cCI6MTcyMTc2MjUwMX0.CqSsdV2Tte2gXbgE-VLfG1qLza7MpZF798fMhFCnH78'
